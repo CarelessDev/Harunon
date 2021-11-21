171dd4f624b3112a9e8c6fc625f04ab2bcf21c43
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-11-21 13:53:40 +0700

    Ping Pong in message

--- cogs/Legacy/Haru.py
+++ cogs/Legacy/Haru.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from datetime import datetime
 
 
 class Haru(commands.Cog):
@@ -9,3 +10,11 @@ class Haru(commands.Cog):
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
         await ctx.send("An error occurred: {}".format(str(error)))
+
+    @commands.command(name="ping")
+    async def _ping(self, ctx: commands.Context):
+        """Ping Pong 遊ぼう!"""
+        interval = datetime.utcnow() - ctx.message.created_at
+        await ctx.send(
+            f"Pong! Ping = {interval.total_seconds() * 1000} ms"
+        )
