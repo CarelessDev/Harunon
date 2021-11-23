0fee16d6886dc271ab5610be033e7723030738d0
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-11-23 18:42:19 +0700

    Fix Error

--- utils/slash.py
+++ utils/slash.py
@@ -1,10 +1,10 @@
-from discord_slash.utils.manage_commands import create_choice, SlashCommandOptionType as SOT
+from discord_slash.utils.manage_commands import create_option, SlashCommandOptionType as SOT
 
 
 class SlashUtils:
     @staticmethod
     def ephemeral(text: str = "Reduce mess caused to channel"):
-        return create_choice(
+        return create_option(
             name="ephemeral",
             description=text,
             required=False,
